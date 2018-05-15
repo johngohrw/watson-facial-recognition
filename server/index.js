df047d925b22ddfd1524ca1d837de49395696e2b
John Goh <john@accendo.com.my>
2018-05-15 15:20:02 +0800

    make getGenderList and numberOfFaces as a function

--- server/index.js
+++ server/index.js
@@ -6,10 +6,6 @@ const faces = require('./faces.js');
 io.on('connection', (socket) => {
     console.log('new client connection');
 
-    socket.on('boom', () => {
-        console.log('boom');
-    })
-
     var uploader = new SocketIOFile(socket, {
 		uploadDir: 'data',							// simple directory
 		accepts: ['image/jpeg', 'image/png'],		// chrome and some of browsers checking mp3 as 'audio/mp3', not 'audio/mpeg'
@@ -17,36 +13,51 @@ io.on('connection', (socket) => {
 		chunkSize: 10240,							// default is 10240(1KB)
 		transmissionDelay: 0,						// delay of each transmission, higher value saves more cpu resources, lower upload speed. default is 0(no delay)
 		overwrite: true 							// overwrite file if exists, default is true.
-	});
+    });
+    
 	uploader.on('start', (fileInfo) => {
 		console.log('Start uploading');
 		console.log(fileInfo);
-	});
+    });
+    
 	uploader.on('stream', (fileInfo) => {
 		console.log(`${fileInfo.wrote} / ${fileInfo.size} byte(s)`);
-	});
+    });
+    
 	uploader.on('complete', (fileInfo) => {
 		console.log('Upload Complete.');
-		console.log(fileInfo);
+        console.log(fileInfo);
+
         faces.vrRequest(fileInfo.uploadDir, (response) => {
             console.log(JSON.stringify(response, null, 2));
-            var gender = []
-            var totalFaces = response.images[0].faces.length
-            console.log(totalFaces)
+            let totalFaces = numberOfFaces(response);
+            let genderList = getGenderList(response);
             
-            for (let i = 0; i < totalFaces; i++){
-              let view = response.images[0].faces[i].gender.gender
-              console.log(view)
-              gender.push(view)
-              console.log(i)           
-            }
+            console.log(genderList);
         });
-	});
+    });
+    
 	uploader.on('error', (err) => {
 		console.log('Error!', err);
-	});
+    });
+    
 	uploader.on('abort', (fileInfo) => {
 		console.log('Aborted: ', fileInfo);
     });
 });
 
+// count number of faces
+const numberOfFaces = (response) => {
+    return response.images[0].faces.length;
+}
+
+// check genders in image
+const getGenderList = (response) => {
+    let glist = [];
+    let totalFaces = numberOfFaces(response);
+    for (let i = 0; i < totalFaces; i++){
+        let thisGender = response.images[0].faces[i].gender.gender
+        glist.push(thisGender)
+    };
+    return glist;
+}
