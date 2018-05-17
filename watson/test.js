bd9ba1dd42ccaae90a345d3765a22aa99e41be72
TheGreatGaben <gdtan3@student.monash.edu>
2018-05-17 14:04:48 +0800

    unit testing for average age

--- watson/test.js
+++ watson/test.js
@@ -5,6 +5,7 @@ const faces = require('./faces.js');
 const functions = require('./functions.js');
 const networkTimeout = 100000;
 
+
 describe("VisualRecognition", function () {
     it("numberOfFaces() returns the number of faces in the given image",
         function (done) {
@@ -25,6 +26,32 @@ describe("VisualRecognition", function () {
             faces.vrRequest(filePath, (response) => {
                 const totalfaces = functions.numberOfFaces(response);
                 expect(totalfaces).to.equal(0);
+                done();
+            });
+        });
+
+
+    it("getAverageAge() returns the overall age average from the faces detected",
+        function (done) {
+            this.timeout(networkTimeout);
+            const filePath = "./test_images/test1.jpg";
+            faces.vrRequest(filePath, (response) => {
+                const totalfaces = functions.numberOfFaces(response);
+                const averageAge = functions.getAverageAge(response, totalfaces);
+                expect(averageAge).to.be.within(20, 30);
+                done();
+            });
+        });
+
+
+    it("getAverageAge() returns 0 if no faces detected",
+        function (done) {
+            this.timeout(networkTimeout);
+            const filePath = "./test_images/test2.jpg";
+            faces.vrRequest(filePath, (response) => {
+                const totalfaces = functions.numberOfFaces(response);
+                const averageAge = functions.getAverageAge(response, totalfaces);
+                expect(averageAge).to.equal(0);
                 done();
             });
         });
